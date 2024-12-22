a9be88166c32b7fbece9e6a128812e5420011c7d
Adrien Suau <adrien.suau+github@protonmail.com>
2024-12-22 15:06:50 +0000

    Remove main.py from tracked files

--- main.py
+++
@@ -1,7 +0,0 @@
-from tqec.templates.indices.display import display_template_from_instantiation
-from tqec.templates.indices.qubit import QubitSpatialJunctionTemplate
-
-template = QubitSpatialJunctionTemplate()
-array = template.instantiate(2)
-print(array)
-display_template_from_instantiation(array)
