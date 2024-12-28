cf0146c253fb99a125c4f0d4ffc8565f17377cb7
Kabir Dubey <kabir@u.northwestern.edu>
2024-12-28 15:04:19 -0800

    deleted add_inset

--- src/tqec/simulation/__init__.py
+++ src/tqec/simulation/__init__.py
@@ -1,5 +1,4 @@
 """Defines several helper methods to facilitate QEC circuit simulation."""
 
-from .plotting import add_inset_axes3d as add_inset_axes3d
 from .plotting import plot_observable_as_inset as plot_observable_as_inset
 from .simulation import start_simulation_using_sinter as start_simulation_using_sinter
