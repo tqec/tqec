4f47b47181109925492058d9aa23d5e5cf75214b
Kabir Dubey <kabir@u.northwestern.edu>
2024-12-28 14:46:47 -0800

    deleted semi-private modules in simulation

--- src/tqec/simulation/__init__.py
+++ src/tqec/simulation/__init__.py
@@ -2,8 +2,4 @@
 
 from .plotting import add_inset_axes3d as add_inset_axes3d
 from .plotting import plot_observable_as_inset as plot_observable_as_inset
-from .generation import (
-    generate_stim_circuits_with_detectors as generate_stim_circuits_with_detectors,
-)
-from .generation import generate_sinter_tasks as generate_sinter_tasks
 from .simulation import start_simulation_using_sinter as start_simulation_using_sinter
