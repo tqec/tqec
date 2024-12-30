cdab61a8c2003dcb83c6ba0ab01c2699de3377cb
Adrien Suau <adrien.suau+github@protonmail.com>
2024-12-30 16:05:20 +0000

    Update CSS plaquette compilation

--- src/tqec/plaquette/compilation/css.py
+++ src/tqec/plaquette/compilation/css.py
@@ -1,10 +1,14 @@
 from tqec.plaquette.compilation.base import PlaquetteCompiler
+from tqec.plaquette.compilation.passes.controlled_gate_basis import (
+    ChangeControlledGateBasisPass,
+)
 from tqec.plaquette.compilation.passes.measurement_basis import (
     ChangeMeasurementBasisPass,
 )
 from tqec.plaquette.compilation.passes.reset_basis import ChangeResetBasisPass
 from tqec.plaquette.compilation.passes.scheduling import ChangeSchedulePass, ScheduleMap
-from tqec.plaquette.enums import MeasurementBasis, ResetBasis
+from tqec.plaquette.compilation.passes.transformer import ScheduleConstant
+from tqec.plaquette.enums import Basis, MeasurementBasis, ResetBasis
 
 CSSPlaquetteCompiler = PlaquetteCompiler(
     "CSS",
@@ -12,7 +16,10 @@ CSSPlaquetteCompiler = PlaquetteCompiler(
         # Move schedules to have an empty schedule for basis change after resets
         # and before measurements.
         ChangeSchedulePass(ScheduleMap({0: 0, 6: 8} | {i: i + 1 for i in range(1, 6)})),
-        ChangeResetBasisPass(ResetBasis.Z),
-        ChangeMeasurementBasisPass(MeasurementBasis.Z),
+        ChangeResetBasisPass(Basis.Z),
+        ChangeMeasurementBasisPass(Basis.Z),
+        ChangeControlledGateBasisPass(
+            Basis.X, ScheduleConstant(1), ScheduleConstant(7)
+        ),
     ],
 )
