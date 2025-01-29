2f338a8201b8d245596b9705fd3ceeb612fbb4b9
Adrien Suau <adrien.suau+github@protonmail.com>
2025-01-29 19:31:09 +0000

    Remove Plaquette.from_rpng due to cyclic import

--- src/tqec/plaquette/plaquette.py
+++ src/tqec/plaquette/plaquette.py
@@ -10,9 +10,6 @@ from typing_extensions import override
 from tqec.circuit.schedule import ScheduledCircuit
 from tqec.plaquette.enums import PlaquetteOrientation
 from tqec.plaquette.qubit import PlaquetteQubits
-from tqec.plaquette.rpng.rpng import RPNGDescription
-from tqec.plaquette.rpng.translators.base import RPNGTranslator
-from tqec.plaquette.rpng.translators.default import DefaultRPNGTranslator
 from tqec.utils.exceptions import TQECException
 from tqec.utils.frozendefaultdict import FrozenDefaultDict
 from tqec.utils.position import PhysicalQubitPosition2D
@@ -119,13 +116,6 @@ class Plaquette:
     @property
     def num_measurements(self) -> int:
         return self.circuit.num_measurements
-
-    @staticmethod
-    def from_rpng(
-        description: RPNGDescription,
-        translator: RPNGTranslator = DefaultRPNGTranslator(),
-    ) -> Plaquette:
-        return translator.translate(description)
 
 
 @dataclass(frozen=True)
