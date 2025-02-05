c9cea5c82cfe80b348ec99313e77e91bc1bd99d9
Adrien Suau <adrien.suau+github@protonmail.com>
2025-02-05 12:48:48 +0000

    LayoutLayer is more generic (now accepts RawCircuitLayer as well)

--- src/tqec/blocks/layers/atomic/layout.py
+++ src/tqec/blocks/layers/atomic/layout.py
@@ -1,31 +1,57 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Iterable
 
 from typing_extensions import override
 
 from tqec.blocks.enums import SpatialBlockBorder
 from tqec.blocks.layers.atomic.base import BaseLayer
-from tqec.plaquette.plaquette import Plaquettes
-from tqec.templates.layout import LayoutTemplate
 from tqec.utils.exceptions import TQECException
+from tqec.utils.position import BlockPosition2D
 from tqec.utils.scale import PhysicalQubitScalable2D
 
 
-@dataclass
+@dataclass(frozen=True)
 class LayoutLayer(BaseLayer):
-    """Represents a layer with a spatial footprint that is defined by a
-    :class:`~tqec.templates.layout.LayoutTemplate` instance.
-    """
+    """A layer gluing several other layers together on a 2-dimensional grid."""
 
-    template: LayoutTemplate
-    plaquettes: Plaquettes
+    layers: dict[BlockPosition2D, BaseLayer]
+
+    def __post_init__(self) -> None:
+        if not self.layers:
+            clsname = self.__class__.__name__
+            raise TQECException(
+                f"An instance of {clsname} should have at least one layer."
+            )
+        scalable_shapes = frozenset(
+            layer.scalable_shape for layer in self.layers.values()
+        )
+        if len(scalable_shapes) > 1:
+            raise TQECException(
+                "Found different scalable shapes in the provided layers: "
+                f"{scalable_shapes}. This is not permitted."
+            )
+
+    @cached_property
+    def _block_width(self) -> BlockPosition2D:
+        positions = list(self.layers.keys())
+        xs, ys = [p.x for p in positions], [p.y for p in positions]
+        return BlockPosition2D(max(xs) - min(xs), max(ys) - min(ys))
+
+    @cached_property
+    def _element_shape(self) -> PhysicalQubitScalable2D:
+        return next(iter(self.layers.values())).scalable_shape
 
     @property
     @override
     def scalable_shape(self) -> PhysicalQubitScalable2D:
-        return self.template.scalable_shape * self.template._default_shift
+        block_width = self._block_width
+        element_shape = self._element_shape
+        return PhysicalQubitScalable2D(
+            block_width.x * element_shape.x, block_width.y * element_shape.y
+        )
 
     @override
     def with_spatial_borders_trimed(
