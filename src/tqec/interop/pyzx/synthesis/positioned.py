92de11b3ea71a70199f6e0a47f4fd9256c19ee9e
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-02-05 20:30:17 +0800

    Add block synthesis from positioned zx graph

---
+++ src/tqec/interop/pyzx/synthesis/positioned.py
@@ -0,0 +1,239 @@
+"""Mapping from a positioned ZX graph to a block graph."""
+
+from typing import cast
+
+import pyzx as zx
+from pyzx.utils import vertex_is_zx
+
+from tqec.computation.block_graph import BlockGraph
+from tqec.computation.cube import Cube, Port, YCube, ZXCube
+from tqec.computation.pipe import PipeKind
+from tqec.utils.enums import Basis
+from tqec.utils.exceptions import TQECException
+from tqec.utils.position import Direction3D
+from tqec.interop.pyzx.positioned import PositionedZX
+from tqec.interop.pyzx.utils import is_zx_no_phase, is_boundary
+
+
+def positioned_block_synthesis(g: PositionedZX) -> BlockGraph:
+    """Convert a positioned ZX graph to a :py:class:`~tqec.computation.block_graph.BlockGraph`.
+
+    This strategy requires specifying the 3D positions of each vertex explicitly
+    in the ZX graph. Then the conversion converts each vertex by looking at its
+    nearest neighbors to infer the cube and pipe kind. The conversion maps each
+    vertex to a cube in the block graph and each edge to a pipe connecting the
+    corresponding cubes.
+
+    The conversion process is as follows:
+
+    1. Construct blocks for all the L/T/X-shape subgraphs in the ZX graph.
+    2. Construct pipes connecting ports/Y to ports/Y nodes.
+    3. Greedily construct the pipes until no more pipes can be inferred.
+    4. If there are still nodes left, then choose orientation for an arbitrary node
+       and repeat step 3 and 4 until all nodes are handled or conflicts are detected.
+
+    Args:
+        g: The positioned ZX graph to be converted to a block graph.
+
+    Returns:
+        The :py:class:`~tqec.computation.block_graph.BlockGraph` object converted from the ZX graph.
+
+    Raises:
+        TQECException: A valid block graph cannot be constructed.
+    """
+    nodes_to_handle = set(g.g.vertices())
+    edges_to_handle = set(g.g.edges())
+
+    bg = BlockGraph()
+    # 1. Construct cubes for all the corner nodes in the ZX graph.
+    _handle_corners(g, bg, nodes_to_handle)
+
+    # 2. Construct pipes connecting ports/Y to ports/Y nodes.
+    _handle_special_pipes(g, bg, nodes_to_handle, edges_to_handle)
+
+    # 3. Greedily construct the pipes until no more pipes can be inferred.
+    _greedily_construct_blocks(g, bg, nodes_to_handle, edges_to_handle)
+
+    # 4. If there are still nodes left, then choose orientation for an arbitrary node
+    # and repeat 3. Repeat 4 until all nodes are handled or conflicts are detected.
+    _handle_leftover_nodes(g, bg, nodes_to_handle, edges_to_handle)
+
+    # Sanity check for the block graph
+    bg.validate()
+    return bg
+
+
+def _handle_corners(
+    pg: PositionedZX, bg: BlockGraph, nodes_to_handle: set[int]
+) -> None:
+    g = pg.g
+    for v in g.vertices():
+        directions = {pg.get_direction(u, v) for u in g.neighbors(v)}
+        if len(directions) != 2:
+            continue
+        normal_direction = (
+            set(Direction3D.all_directions()).difference(directions).pop()
+        )
+        normal_direction_basis = Basis.Z if g.type(v) == zx.VertexType.Z else Basis.X
+        bases = [normal_direction_basis.flipped() for _ in range(3)]
+        bases[normal_direction.value] = normal_direction_basis
+        kind = ZXCube(*bases)
+        bg.add_node(Cube(pg[v], kind))
+        nodes_to_handle.remove(v)
+
+
+def _handle_special_pipes(
+    pg: PositionedZX,
+    bg: BlockGraph,
+    nodes_to_handle: set[int],
+    edges_to_handle: set[tuple[int, int]],
+) -> None:
+    g = pg.g
+
+    for edge in set(edges_to_handle):
+        u, v = edge
+        if is_zx_no_phase(g, u) or is_zx_no_phase(g, v):
+            continue
+        bg.add_edge(
+            _port_or_y_cube(pg, u),
+            _port_or_y_cube(pg, v),
+            _choose_arbitrary_pipe_kind(pg, edge),
+        )
+        nodes_to_handle.remove(u)
+        nodes_to_handle.remove(v)
+        edges_to_handle.remove(edge)
+
+
+def _greedily_construct_blocks(
+    pg: PositionedZX,
+    bg: BlockGraph,
+    nodes_to_handle: set[int],
+    edges_to_handle: set[tuple[int, int]],
+) -> None:
+    num_nodes_left = len(nodes_to_handle) + 1
+    while len(nodes_to_handle) < num_nodes_left:
+        num_nodes_left = len(nodes_to_handle)
+        _try_to_handle_edges(pg, bg, nodes_to_handle, edges_to_handle)
+
+
+def _try_to_handle_edges(
+    pg: PositionedZX,
+    bg: BlockGraph,
+    nodes_to_handle: set[int],
+    edges_to_handle: set[tuple[int, int]],
+) -> None:
+    g = pg.g
+    for edge in set(edges_to_handle):
+        u, v = edge
+        if u in nodes_to_handle and v in nodes_to_handle:
+            continue
+        ut, vt = g.type(u), g.type(v)
+        can_infer_from_u = u not in nodes_to_handle and is_zx_no_phase(g, u)
+        can_infer_from_v = v not in nodes_to_handle and is_zx_no_phase(g, v)
+        if not can_infer_from_u and not can_infer_from_v:
+            continue
+        infer_from, other_node = (u, v) if can_infer_from_u else (v, u)
+        ipos, opos = pg[infer_from], pg[other_node]
+        cube_kind = cast(ZXCube, bg[ipos].kind)
+        pipe_kind = PipeKind._from_cube_kind(
+            cube_kind,
+            pg.get_direction(u, v),
+            can_infer_from_u,
+            g.edge_type(edge) == zx.EdgeType.HADAMARD,
+        )
+        other_cube: Cube
+        if is_zx_no_phase(g, other_node):
+            other_cube_kind = _infer_cube_kind_from_pipe(
+                pipe_kind, not can_infer_from_u, vt if can_infer_from_u else ut
+            )
+            # Check cube kind conflicts
+            if other_node not in nodes_to_handle:
+                existing_kind = bg[opos].kind
+                if not other_cube_kind == existing_kind:
+                    raise TQECException(f"Encounter conflicting cube kinds at {opos}: ")
+            other_cube = Cube(opos, other_cube_kind)
+        else:
+            other_cube = _port_or_y_cube(pg, other_node)
+        bg.add_edge(Cube(ipos, cube_kind), other_cube)
+        if other_node in nodes_to_handle:
+            nodes_to_handle.remove(other_node)
+        edges_to_handle.remove(edge)
+
+
+def _fix_kind_for_one_node(
+    pg: PositionedZX,
+    bg: BlockGraph,
+    nodes_to_handle: set[int],
+) -> None:
+    g = pg.g
+    sorted_nodes = sorted(nodes_to_handle, key=lambda n: pg[n])
+    fix_node = next(n for n in sorted_nodes if is_zx_no_phase(g, n))
+    fix_pos = pg[fix_node]
+    fix_type = g.type(fix_node)
+    # Special case: single node ZXGraph
+    if g.vertex_degree(fix_node) == 0:
+        specified_kind = (
+            ZXCube.from_str("ZXZ")
+            if fix_type == zx.VertexType.X
+            else ZXCube.from_str("ZXX")
+        )
+    else:
+        # the basis along the edge direction must be the opposite of the node kind
+        basis = ["X", "Z"]
+        neighbor = next(iter(g.neighbors(fix_node)))
+        basis.insert(
+            pg.get_direction(fix_node, neighbor).value,
+            "X" if fix_type == zx.VertexType.Z else "Z",
+        )
+        specified_kind = ZXCube.from_str("".join(basis))
+    bg.add_node(Cube(fix_pos, specified_kind))
+    nodes_to_handle.remove(fix_node)
+
+
+def _handle_leftover_nodes(
+    pg: PositionedZX,
+    bg: BlockGraph,
+    nodes_to_handle: set[int],
+    edges_to_handle: set[tuple[int, int]],
+) -> None:
+    while nodes_to_handle:
+        _fix_kind_for_one_node(pg, bg, nodes_to_handle)
+        _greedily_construct_blocks(pg, bg, nodes_to_handle, edges_to_handle)
+
+
+def _choose_arbitrary_pipe_kind(pg: PositionedZX, edge: tuple[int, int]) -> PipeKind:
+    bases: list[str] = ["X", "Z"]
+    direction = pg.get_direction(*edge)
+    bases.insert(direction.value, "O")
+    if pg.g.edge_type(edge) == zx.EdgeType.HADAMARD:
+        bases.append("H")
+    return PipeKind.from_str("".join(bases))
+
+
+def _infer_cube_kind_from_pipe(
+    pipe_kind: PipeKind,
+    at_pipe_head: bool,
+    vertex_type: zx.VertexType,
+) -> ZXCube:
+    """Infer the cube kinds from the pipe kind."""
+    bases = [
+        pipe_kind.get_basis_along(direction, at_pipe_head)
+        for direction in Direction3D.all_directions()
+    ]
+    assert vertex_is_zx(vertex_type)
+    bases[pipe_kind.direction.value] = (
+        Basis.Z if vertex_type == zx.VertexType.X else Basis.X
+    )
+    return ZXCube(*cast(list[Basis], bases))
+
+
+def _port_or_y_cube(pg: PositionedZX, v: int) -> Cube:
+    g = pg.g
+    if is_boundary(g, v):
+        if v in g.inputs():
+            label = f"IN_{g.inputs().index(v)}"
+        else:
+            assert v in g.outputs()
+            label = f"OUT_{g.outputs().index(v)}"
+        return Cube(pg[v], Port(), label)
+    return Cube(pg[v], YCube())
---
+++ src/tqec/interop/pyzx/synthesis/positioned_test.py
@@ -0,0 +1,123 @@
+from fractions import Fraction
+
+from pyzx import VertexType
+
+from tqec.computation.block_graph import BlockGraph
+from tqec.computation.cube import Cube, Port, YCube, ZXCube
+from tqec.computation.pipe import PipeKind
+from tqec.interop.pyzx._testing import make_positioned_zx_graph
+from tqec.interop.pyzx.synthesis.positioned import positioned_block_synthesis
+from tqec.utils.position import Position3D
+
+
+def test_one_to_one_synthesis_single_zx_edge() -> None:
+    g = make_positioned_zx_graph(
+        vertex_types=[VertexType.Z, VertexType.X],
+        positions=[
+            Position3D(0, 0, 0),
+            Position3D(1, 0, 0),
+        ],
+        edges=[(0, 1)],
+        hadamard_edges=[False],
+    )
+    block = positioned_block_synthesis(g)
+    expected_block = BlockGraph()
+    expected_block.add_edge(
+        Cube(Position3D(0, 0, 0), ZXCube.from_str("XXZ")),
+        Cube(Position3D(1, 0, 0), ZXCube.from_str("ZXZ")),
+    )
+    assert block == expected_block
+
+    g = make_positioned_zx_graph(
+        vertex_types=[VertexType.Z, VertexType.Z],
+        positions=[
+            Position3D(0, 0, 0),
+            Position3D(0, 0, 1),
+        ],
+        edges=[(0, 1)],
+        hadamard_edges=[False],
+    )
+    block = positioned_block_synthesis(g)
+    expected_block = BlockGraph()
+    expected_block.add_edge(
+        Cube(Position3D(0, 0, 0), ZXCube.from_str("XZX")),
+        Cube(Position3D(0, 0, 1), ZXCube.from_str("XZX")),
+    )
+    assert block == expected_block
+
+
+def test_one_to_one_synthesis_single_y_p_edge() -> None:
+    g = make_positioned_zx_graph(
+        vertex_types=[VertexType.Z, VertexType.BOUNDARY],
+        positions=[
+            Position3D(0, 0, 0),
+            Position3D(0, 0, 1),
+        ],
+        phases={0: Fraction(1, 2)},
+        edges=[(0, 1)],
+        hadamard_edges=[False],
+        outputs=(1,),
+    )
+    block = positioned_block_synthesis(g)
+    expected_block = BlockGraph()
+    expected_block.add_edge(
+        Cube(Position3D(0, 0, 0), YCube()),
+        Cube(Position3D(0, 0, 1), Port(), "OUT_0"),
+        PipeKind.from_str("XZO"),
+    )
+    assert block == expected_block
+
+
+def test_one_to_one_synthesis_L_shape() -> None:
+    g = make_positioned_zx_graph(
+        vertex_types=[VertexType.Z, VertexType.BOUNDARY, VertexType.BOUNDARY],
+        positions=[
+            Position3D(0, 0, 0),
+            Position3D(0, 0, 1),
+            Position3D(1, 0, 0),
+        ],
+        edges=[(0, 1), (0, 2)],
+        hadamard_edges=[False, False],
+        inputs=(1,),
+        outputs=(2,),
+    )
+    block = positioned_block_synthesis(g)
+    expected_block = BlockGraph()
+    expected_block.add_edge(
+        Cube(Position3D(0, 0, 0), ZXCube.from_str("XZX")),
+        Cube(Position3D(0, 0, 1), Port(), "IN_0"),
+    )
+    expected_block.add_edge(
+        Cube(Position3D(0, 0, 0), ZXCube.from_str("XZX")),
+        Cube(Position3D(1, 0, 0), Port(), "OUT_0"),
+    )
+    assert block == expected_block
+
+
+def test_one_to_one_synthesis_X_shape() -> None:
+    ports = [
+        Position3D(1, 0, 0),
+        Position3D(0, 1, 0),
+        Position3D(-1, 0, 0),
+        Position3D(0, -1, 0),
+    ]
+    g = make_positioned_zx_graph(
+        vertex_types=[VertexType.Z] + [VertexType.BOUNDARY] * 4,
+        positions=[Position3D(0, 0, 0)] + ports,
+        edges=[(0, 1), (0, 2), (0, 3), (0, 4)],
+        hadamard_edges=[False] * 4,
+        inputs=(1,),
+        outputs=(2, 3, 4),
+    )
+    block = positioned_block_synthesis(g)
+    expected_block = BlockGraph()
+    expected_block.add_edge(
+        Cube(Position3D(0, 0, 0), ZXCube.from_str("XXZ")),
+        Cube(ports[0], Port(), "IN_0"),
+    )
+    for i, port in enumerate(ports[1:]):
+        expected_block.add_edge(
+            Cube(Position3D(0, 0, 0), ZXCube.from_str("XXZ")),
+            Cube(port, Port(), f"OUT_{i}"),
+        )
+    assert block == expected_block
