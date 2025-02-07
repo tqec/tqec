5e916baa83eff40ddf632752b669b643b4da3732
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-02-07 16:55:39 +0800

    Delete docs about `ZXGraph`

--- src/tqec/computation/__init__.py
+++ src/tqec/computation/__init__.py
@@ -1,15 +1,8 @@
 """Defines the data structures to represent the logical computations.
 
 This module provides high-level abstractions to represent the fault-tolerant logical
-computations protected by surface code. There are two representations of the logical
-computation:
-
-- :py:class:`~tqec.computation.zx_graph.ZXGraph`: A restricted form of the ZX diagram from
-  the ZX-calculus.
-- :py:class:`~tqec.computation.block_graph.BlockGraph`: A graph consisting of individual
-  building blocks that fully specify the boundary conditions and topological structures
-  of the logical computation. It corresponds to the commonly used 3D spacetime diagram
-  representation of a surface code logical computation.
+computations protected by surface code. It defines the core representation of the
+logical computation: :py:class:`~tqec.computation.block_graph.BlockGraph`:.
 """
 
 from tqec.computation.block_graph import BlockGraph as BlockGraph
