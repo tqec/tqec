dc762bcc0fc7c60aebfddb56fc17066c61ea75b9
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-02-07 18:37:47 +0800

    Fix some docs

--- src/tqec/computation/correlation.py
+++ src/tqec/computation/correlation.py
@@ -16,12 +16,36 @@ if TYPE_CHECKING:
 
 @dataclass(frozen=True, order=True)
 class ZXNode:
+    """Represent a node in the ZX graph spanned by the correlation surface.
+
+    Correlation surface is represented by a set of edges in the ZX graph. Each edge
+    consists of two nodes. The node id refers to the vertex index in the graph
+    and the basis represents the Pauli operator on the half edge incident to the node.
+
+    Attributes:
+        id: The index of the vertex in the graph.
+        basis: The Pauli operator on the half edge incident to the node.
+    """
+
     id: int
     basis: Basis
 
 
 @dataclass(frozen=True, order=True)
 class ZXEdge:
+    """Represent an edge in the ZX graph spanned by the correlation surface.
+
+    Correlation surface is represented by a set of edges in the ZX graph. Each edge
+    consists of two nodes. The edge can be decomposed into two half edges, each
+    incident to a node. The half edges are labeled with the Pauli operator.
+    If the edge has no hadamard effect, the Pauli operators on the two half edges
+    should be the same. Otherwise, the Pauli operators should be flipped.
+
+    Note that the Pauli operators are represented by `Basis` enum, which only
+    contains `X` and `Z` operators. The `Y` operator is represented by two
+    edges with `X` and `Z` operators respectively.
+    """
+
     u: ZXNode
     v: ZXNode
 
@@ -36,19 +60,50 @@ class ZXEdge:
         yield self.v
 
     def is_self_loop(self) -> bool:
+        """Whether the edge is a self-loop edge.
+
+        By definition, a self-loop edge represents a correlation surface within
+        a single node. This is an edge case where the ZX graph only contains a
+        single node.
+        """
         return self.u.id == self.v.id
 
     @property
     def has_hadamard(self) -> bool:
+        """Whether the edge has a hadamard effect."""
         return self.u.basis != self.v.basis
 
 
 @dataclass(frozen=True)
 class CorrelationSurface:
+    """A correlation surface in a computation is a set of measurements whose
+    values determine the parity of the logical operators at the inputs and
+    outputs associated with the surface.
+
+    Note:
+        We use the term "correlation surface", "pauli web" and "observable" interchangeably in
+        the library.
+
+    Here we represent the correlation surface by the set of edges it spans in the ZX graph.
+    The insight is that the spiders pose parity constraints on the operators supported on
+    the incident edges. The flow of the logical operators through the ZX graph, respecting
+    the parity constraints, forms the correlation between the inputs and outputs. However,
+    the sign of measurement outcomes is neglected in this representation. And we need to
+    recover the measurements when instantiating an explicit logical observable from the
+    correlation surface.
+
+    Each edge establishes a correlation between the logical operators at the two ends of
+    the edge. For example, an edge connecting two Z nodes represents the correlation
+    between the logical Z operators at the two nodes.
+
+    Attributes:
+        span: A set of ``ZXEdge`` representing the span of the correlation surface.
+    """
+
     span: frozenset[ZXEdge]
 
     def bases_at(self, v: int) -> set[Basis]:
-        """Get the bases of the present surfaces at the vertex."""
+        """Get the bases of the surfaces present at the vertex."""
         edges = self.edges_at(v)
         bases = set()
         for edge in edges:
