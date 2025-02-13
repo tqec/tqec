6d949c63b7af0fb717829b60e6ea145ad4a6b882
J <80164315+jbolns@users.noreply.github.com>
2025-02-13 19:31:34 +0200

    Debugged rotations test

--- examples/rotations_test.py
+++ examples/rotations_test.py
@@ -8,60 +8,59 @@ One of the models is unrotated, the other is rotated 90 degrees along the 'x' ax
 import matplotlib.pyplot as plt
 import numpy as np
 import sinter
+from pathlib import Path
 from multiprocessing import cpu_count
 from tqec import BlockGraph, compile_block_graph
 from tqec.utils.noise_model import NoiseModel
 from tqec.simulation.simulation import start_simulation_using_sinter
 from tqec.simulation.plotting.inset import plot_observable_as_inset
+from tqec import SignedDirection3D, Direction3D
 
+# FOLDERS
+EXAMPLE_FOLDER = Path(__file__).parent
+TQEC_FOLDER = EXAMPLE_FOLDER.parent
+ASSETS_FOLDER = TQEC_FOLDER / "assets"
 
 # IMPORT / CONSTRUCT LOGICAL COMPUTATIONS
 # To use the computation with the tqec library, you need to import it using tqec.BlockGraph
+# The tqec library can automatically search for valid observables in the imported computation.
+# To get a list of all the valid observables, you can use the following code
+filename = "rotated_cnot_x_180"
+
+
 def write_to_html(filename, html):
     with open(f"{filename}.html", "w") as f:
         f.write(str(html))
         f.close()
 
 
-block_graph = BlockGraph.from_dae_file("../assets/logical_cnot.dae")
-write_to_html("logical_cnot", block_graph.view_as_html())
-
-block_graph_rotated = BlockGraph.from_dae_file("../assets/cnot_rotate_x_90.dae")
-write_to_html("logical_cnot_rotated", block_graph_rotated.view_as_html())
-
-
-# CHOOSE OBSERVABLES OF INTEREST
-# The tqec library can automatically search for valid observables in the imported computation.
-# To get a list of all the valid observables, you can use the following code
+# block_graph = BlockGraph.from_dae_file("assets/logical_cnot.dae")
+block_graph = BlockGraph.from_dae_file(ASSETS_FOLDER / f"{filename}.dae")
 correlation_surfaces = block_graph.find_correlation_surfaces()
-correlation_surfaces_rotated = block_graph_rotated.find_correlation_surfaces()
-
+write_to_html(
+    filename,
+    block_graph.view_as_html(
+        pop_faces_at_direction=SignedDirection3D(Direction3D.Z, True),
+        show_correlation_surface=correlation_surfaces[1],
+    ),
+)
 
 # COMPILE & EXPORT COMPUTATION
 # to get a stim.Circuit instance, the computation first need to be compiled.
 compiled_computation = compile_block_graph(
     block_graph, observables=[correlation_surfaces[1]]
 )
-compiled_computation_rotated = compile_block_graph(
-    block_graph, observables=[correlation_surfaces[1]]
-)
-
+print(compiled_computation)
 
 # GENERATE STIM CIRCUIT OF TARGET CODE DISTANCE
 circuit = compiled_computation.generate_stim_circuit(
-    # k = (d-1)/2 is the scale factor. Large values are computationally intensive.
+    # k = (d-1)/2 is the scale factor
+    # Large values will take a lot of time.
     k=2,
     # The noise applied and noise levels can be changed.
     noise_model=NoiseModel.uniform_depolarizing(0.001),
 )
-
-circuit_rotated = compiled_computation_rotated.generate_stim_circuit(
-    # k = (d-1)/2 is the scale factor. Large values are computationally intensive.
-    k=2,
-    # The noise applied and noise levels can be changed.
-    noise_model=NoiseModel.uniform_depolarizing(0.001),
-)
-
+print(circuit)
 
 # SIMULATE MULTIPLE EXPERIMENTS
 # Returns a iterator
@@ -72,20 +71,6 @@ stats = start_simulation_using_sinter(
     noise_model_factory=NoiseModel.uniform_depolarizing,  # noise model
     manhattan_radius=2,  # parameter for automatic detector computation
     observables=[correlation_surfaces[1]],  # observable of interest
-    decoders=["pymatching"],
-    num_workers=cpu_count(),
-    max_shots=10_000_000,
-    max_errors=5_000,
-    print_progress=True,
-)
-
-stats_rotated = start_simulation_using_sinter(
-    block_graph,
-    ks=range(1, 4),  # k values for the code distance
-    ps=list(np.logspace(-4, -1, 10)),  # error rates
-    noise_model_factory=NoiseModel.uniform_depolarizing,  # noise model
-    manhattan_radius=2,  # parameter for automatic detector computation
-    observables=[correlation_surfaces_rotated[1]],  # observable of interest
     decoders=["pymatching"],
     num_workers=cpu_count(),
     max_shots=10_000_000,
@@ -111,24 +96,5 @@ plot_observable_as_inset(ax, zx_graph, correlation_surfaces[1])
 ax.grid(axis="both")
 ax.legend()
 ax.loglog()
-ax.set_title("Logical CNOT Error Rate")
-fig.savefig(f"logical_cnot_observable_{1}.png")
-
-
-# ROTATED
-zx_graph = block_graph.to_zx_graph()
-fig, ax = plt.subplots()
-
-sinter.plot_error_rate(
-    ax=ax,
-    stats=next(stats_rotated),
-    x_func=lambda stat: stat.json_metadata["p"],
-    group_func=lambda stat: stat.json_metadata["d"],
-)
-
-plot_observable_as_inset(ax, zx_graph, correlation_surfaces_rotated[1])
-ax.grid(axis="both")
-ax.legend()
-ax.loglog()
-ax.set_title("Rotated logical CNOT Error Rate")
-fig.savefig(f"logical_cnot_rotated_observable_{1}.png")
+ax.set_title(f"Error Rate, {filename}")
+fig.savefig(f"{filename}.png")
