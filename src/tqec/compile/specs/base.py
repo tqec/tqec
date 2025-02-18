9551943d88a0dc6f30a5fd3c7edb94ac397afc5e
Yilun Zhao <40353317+Zhaoyilunnn@users.noreply.github.com>
2025-02-18 20:53:21 +0800

    fix: close #479 (#480)
    1. Fix https://github.com/tqec/tqec/issues/479
    
    As suggested by @inmzhang, move rotation structure includes spatial cube
    and is not supported by the compilation yet.
    
    Now when trying compile `move_rotation`, the output is 
    
    ```
    Traceback (most recent call last):
      File "/home/zhaoyilun/workspace/github/quantum-computing-resources/learning/qec/tqec_learn/test_move_rotation.py", line 8, in <module>
        compiled_computation = compile_block_graph(block_graph, observables="auto")
                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/home/zhaoyilun/workspace/github/forks/tqec/src/tqec/compile/compile.py", line 350, in compile_block_graph
        blocks[cube.position] = block_builder(spec)
                                ^^^^^^^^^^^^^^^^^^^
      File "/home/zhaoyilun/workspace/github/forks/tqec/src/tqec/compile/specs/library/_utils.py", line 29, in default_compiled_block_builder
        raise NotImplementedError("Irregular cubes are not implemented yet.")
    NotImplementedError: Irregular cubes are not implemented yet.
    ```
    
    2. Now `NoiseModel` could be imported directly from `tqec`. The legacy
    README example raises module not found error

--- src/tqec/compile/specs/base.py
+++ src/tqec/compile/specs/base.py
@@ -49,7 +49,7 @@ class CubeSpec:
         pos = cube.position
         spatial_arms = SpatialArms.NONE
         for flag, shift in SpatialArms.get_map_from_arm_to_shift().items():
-            if graph.get_pipe(pos, pos.shift_by(*shift)) is not None:
+            if graph.has_pipe_between(pos, pos.shift_by(*shift)):
                 spatial_arms |= flag
         return CubeSpec(cube.kind, spatial_arms)
 
