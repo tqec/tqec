eeea1315a2f2f5925f9b36b6a8db940ed70e531d
Adrien Suau <adrien.suau+github@protonmail.com>
2025-02-24 11:43:05 +0000

    Remove LayoutLayer dependency on LayoutTemplate

--- src/tqec/compile/blocks/layers/atomic/layout.py
+++ src/tqec/compile/blocks/layers/atomic/layout.py
@@ -8,10 +8,6 @@ from typing_extensions import override
 
 from tqec.compile.blocks.enums import SpatialBlockBorder
 from tqec.compile.blocks.layers.atomic.base import BaseLayer
-from tqec.compile.blocks.layers.atomic.plaquettes import PlaquetteLayer
-from tqec.plaquette.plaquette import Plaquettes
-from tqec.templates.base import RectangularTemplate
-from tqec.templates.layout import LayoutTemplate
 from tqec.utils.exceptions import TQECException
 from tqec.utils.position import BlockPosition2D
 from tqec.utils.scale import PhysicalQubitScalable2D
@@ -39,35 +35,26 @@ class LayoutLayer(BaseLayer):
             )
 
     @cached_property
+    def block_origin(self) -> BlockPosition2D:
+        positions = list(self.layers.keys())
+        xs, ys = [p.x for p in positions], [p.y for p in positions]
+        return BlockPosition2D(min(xs), min(ys))
+
+    @cached_property
     def _block_width(self) -> BlockPosition2D:
         positions = list(self.layers.keys())
         xs, ys = [p.x for p in positions], [p.y for p in positions]
         return BlockPosition2D(max(xs) - min(xs), max(ys) - min(ys))
 
     @cached_property
-    def _element_shape(self) -> PhysicalQubitScalable2D:
+    def element_shape(self) -> PhysicalQubitScalable2D:
         return next(iter(self.layers.values())).scalable_shape
-
-    def to_layout_template_and_plaquettes(self) -> tuple[LayoutTemplate, Plaquettes]:
-        templates: dict[BlockPosition2D, RectangularTemplate] = {}
-        local_plaquettes: dict[BlockPosition2D, Plaquettes] = {}
-        for pos, layer in self.layers.items():
-            if not isinstance(layer, PlaquetteLayer):
-                raise TQECException(
-                    f"Cannot include a layer of type {type(layer)} in a "
-                    "LayoutTemplate instance."
-                )
-            templates[pos] = layer.template
-            local_plaquettes[pos] = layer.plaquettes
-        layout_template = LayoutTemplate(templates)
-        plaquettes = layout_template.global_plaquettes_from_local_ones(local_plaquettes)
-        return layout_template, plaquettes
 
     @property
     @override
     def scalable_shape(self) -> PhysicalQubitScalable2D:
         block_width = self._block_width
-        element_shape = self._element_shape
+        element_shape = self.element_shape
         return PhysicalQubitScalable2D(
             block_width.x * element_shape.x, block_width.y * element_shape.y
         )
