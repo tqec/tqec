a753773b25d4c7fe165c807da4f8f6348d8be902
zhao-yilun-zyl <dilved1014@gmail.com>
2025-02-25 17:02:46 +0800

    fix: only use SquarePlaquetteQubits

--- src/tqec/plaquette/library/spatial_test.py
+++ src/tqec/plaquette/library/spatial_test.py
@@ -2,15 +2,12 @@ import stim
 
 from tqec.utils.enums import Basis
 from tqec.plaquette.library.spatial import make_spatial_cube_arm_plaquette
-from tqec.plaquette.qubit import (
-    UpTrianglePlaquetteQubits,
-    DownTrianglePlaquetteQubits,
-)
+from tqec.plaquette.qubit import SquarePlaquetteQubits
 
 
 def test_spatial_cube_arm_plaquette() -> None:
     plaquette = make_spatial_cube_arm_plaquette(Basis.X, "UP", is_reverse=False)
-    assert plaquette.qubits == UpTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_X_UP"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
@@ -19,6 +16,7 @@ QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
 QUBIT_COORDS(1, -1) 2
 QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RX 0
 RZ 3
 TICK
@@ -33,7 +31,7 @@ CX 3 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.X, "UP", is_reverse=True)
-    assert plaquette.qubits == UpTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_X_UP_REVERSE"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
@@ -42,6 +40,7 @@ QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
 QUBIT_COORDS(1, -1) 2
 QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RX 0
 RZ 3
 TICK
@@ -56,25 +55,26 @@ CX 3 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.X, "DOWN", is_reverse=False)
-    assert plaquette.qubits == DownTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_X_DOWN"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
         """
 QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
-QUBIT_COORDS(-1, 1) 2
-QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(1, -1) 2
+QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RZ 1
 TICK
 RZ 0
 TICK
 CX 1 0
 TICK
-CX 0 2
-TICK
-TICK
 CX 0 3
+TICK
+TICK
+CX 0 4
 TICK
 CX 0 1
 TICK
@@ -82,25 +82,26 @@ MX 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.X, "DOWN", is_reverse=True)
-    assert plaquette.qubits == DownTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_X_DOWN_REVERSE"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
         """
 QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
-QUBIT_COORDS(-1, 1) 2
-QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(1, -1) 2
+QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RZ 1
 TICK
 RZ 0
 TICK
 CX 1 0
 TICK
+CX 0 4
+TICK
+TICK
 CX 0 3
-TICK
-TICK
-CX 0 2
 TICK
 CX 0 1
 TICK
@@ -108,7 +109,7 @@ MX 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.Z, "UP", is_reverse=False)
-    assert plaquette.qubits == UpTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_Z_UP"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
@@ -117,6 +118,7 @@ QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
 QUBIT_COORDS(1, -1) 2
 QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RX 0
 RZ 3
 TICK
@@ -131,7 +133,7 @@ CX 3 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.Z, "UP", is_reverse=True)
-    assert plaquette.qubits == UpTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_Z_UP_REVERSE"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
@@ -140,6 +142,7 @@ QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
 QUBIT_COORDS(1, -1) 2
 QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RX 0
 RZ 3
 TICK
@@ -154,25 +157,26 @@ CX 3 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.Z, "DOWN", is_reverse=False)
-    assert plaquette.qubits == DownTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_Z_DOWN"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
         """
 QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
-QUBIT_COORDS(-1, 1) 2
-QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(1, -1) 2
+QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RZ 1
 TICK
 RZ 0
 TICK
 CX 1 0
 TICK
-CZ 0 2
-TICK
-TICK
 CZ 0 3
+TICK
+TICK
+CZ 0 4
 TICK
 CX 0 1
 TICK
@@ -180,25 +184,26 @@ MX 0
 """
     )
     plaquette = make_spatial_cube_arm_plaquette(Basis.Z, "DOWN", is_reverse=True)
-    assert plaquette.qubits == DownTrianglePlaquetteQubits()
+    assert plaquette.qubits == SquarePlaquetteQubits()
     assert plaquette.name == "SPATIAL_CUBE_ARM_Z_DOWN_REVERSE"
     circuit = plaquette.circuit.get_circuit()
     assert circuit == stim.Circuit(
         """
 QUBIT_COORDS(0, 0) 0
 QUBIT_COORDS(-1, -1) 1
-QUBIT_COORDS(-1, 1) 2
-QUBIT_COORDS(1, 1) 3
+QUBIT_COORDS(1, -1) 2
+QUBIT_COORDS(-1, 1) 3
+QUBIT_COORDS(1, 1) 4
 RZ 1
 TICK
 RZ 0
 TICK
 CX 1 0
 TICK
+CZ 0 4
+TICK
+TICK
 CZ 0 3
-TICK
-TICK
-CZ 0 2
 TICK
 CX 0 1
 TICK
