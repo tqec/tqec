7d4fedf66f1b3b5795bafee4e37c2e919a951fb7
zhao-yilun-zyl <dilved1014@gmail.com>
2025-02-26 15:46:14 +0800

    feat(spatial-cube-arms): make reset and meas on data qubits optional

--- src/tqec/plaquette/library/spatial.py
+++ src/tqec/plaquette/library/spatial.py
@@ -1,4 +1,4 @@
-"""Define the standard CSS-type surface code plaquettes."""
+"""Define the plaquettes for implementing arms for spatial cubes."""
 
 from __future__ import annotations
 
@@ -18,14 +18,32 @@ from tqec.utils.enums import Basis
 def make_spatial_cube_arm_plaquette(
     basis: Basis,
     plaquette_kind: Literal["UP", "DOWN"],
+    reset: Basis | None = None,
+    measurement: Basis | None = None,
     is_reverse: bool = False,
     is_corner_trimmed: bool = False,
 ) -> Plaquette:
     """Make a plaquette for spatial cube arms.
 
+    The below text represents the qubits in a stretched stabilizer ::
+
+        a ----- b
+        |       |
+        |   c   |
+        |       |
+        d ------
+        |       |
+        |   e   |
+        |       |
+        f ----- g
+
+    This is split into two plaquettes, with "UP" being (a, b, c, d) and "DOWN" being (d, e, f, g).
+
     Args:
         basis: the basis of the plaquette.
         plaquette_kind: the kind of the plaquette.
+        reset: the logical basis for data initialization.
+        measurement: the logical basis for data measurement.
         is_reverse: whether the plaquette has controlled-A reversed.
         is_corner_trimmed: whether the plaquette has corner trimmed, for "UP" plaquette
             the left top corner is trimmed, for "DOWN" plaquette the right bottom corner is trimmed.
@@ -33,8 +51,20 @@ def make_spatial_cube_arm_plaquette(
     Returns:
         A plaquette for spatial cube arms.
 
-    References:
-        - Surface code quantum computation by Fowler et al. Fig. 13.
+    Notes:
+        This method could generate 8 different plaquettes.
+        UP-plaquettes:
+        1. Qubits (a, b, c, d)
+        2. Qubits (a, b, c, d) with controlled-A gates reversed
+        DOWN-plaquettes:
+        3. Qubits (d, e, f, g)
+        4. Qubits (d, e, f, g) with controlled-A gates reversed
+        Trimmed UP-plaquettes:
+        5. Qubits (b, c, d)
+        6. Qubits (b, c, d) with controlled-A gates reversed
+        Trimmed DOWN-plaquettes:
+        7. Qubits (d, e, f)
+        8. Qubits (d, e, f) with controlled-A gates reversed
     """
     builder = _SpatialCubeArmPlaquetteBuilder(
         basis,
@@ -42,6 +72,10 @@ def make_spatial_cube_arm_plaquette(
         is_reverse=is_reverse,
         is_corner_trimmed=is_corner_trimmed,
     )
+    if reset is not None:
+        builder.add_data_init_or_meas(reset, False)
+    if measurement is not None:
+        builder.add_data_init_or_meas(measurement, True)
     return builder.build()
 
 
@@ -57,7 +91,7 @@ class _SpatialCubeArmPlaquetteBuilder:
         is_corner_trimmed: bool = False,
     ) -> None:
         self._basis = basis
-        self._plaquette_kind = plaquette_kind
+        self._plaquette_kind: Literal["UP", "DOWN"] = plaquette_kind
         self._is_reverse = is_reverse
         self._is_corner_trimmed = is_corner_trimmed
         self._trimmed_qubit: int | None = None
@@ -76,6 +110,16 @@ class _SpatialCubeArmPlaquetteBuilder:
             | {i + 1: q for i, q in enumerate(self._qubits.data_qubits)}
         )
 
+        self._data_init: Basis | None = None
+        self._data_meas: Basis | None = None
+
+    def _get_data_qubits_for_init_or_meas(self) -> list[int]:
+        match self._plaquette_kind:
+            case "UP":
+                return [2] if self._is_corner_trimmed else [1, 2]
+            case "DOWN":
+                return [3] if self._is_corner_trimmed else [3, 4]
+
     def _get_plaquette_name(self) -> str:
         parts = [
             _SpatialCubeArmPlaquetteBuilder.BASE_NAME,
@@ -86,6 +130,10 @@ class _SpatialCubeArmPlaquetteBuilder:
             parts.append("REVERSE")
         if self._is_corner_trimmed:
             parts.append("CORNER_TRIMMED")
+        if self._data_init is not None:
+            parts.append(f"datainit({self._data_init.name})")
+        if self._data_meas is not None:
+            parts.append(f"datameas({self._data_meas.name})")
         return "_".join(parts)
 
     def build(self) -> Plaquette:
@@ -136,3 +184,15 @@ class _SpatialCubeArmPlaquetteBuilder:
         circuit.append("TICK")
         circuit.append("MX", [0], [])
         return list(iter_stim_circuit_without_repeat_by_moments(circuit))
+
+    def add_data_init_or_meas(self, basis: Basis, is_measurement: bool) -> None:
+        if is_measurement:
+            moment_index = -1
+            self._data_meas = basis
+        else:
+            moment_index = 0
+            self._data_init = basis
+        op = "M" if is_measurement else "R"
+        self._moments[moment_index].append(
+            f"{op}{basis.value}", self._get_data_qubits_for_init_or_meas(), []
+        )
