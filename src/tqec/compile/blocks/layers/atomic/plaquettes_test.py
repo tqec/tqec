8d6703ebe4a85c3f2da534deec241d65fb77bf7b
Adrien Suau <adrien.suau+github@protonmail.com>
2025-02-27 16:01:31 +0000

    Add tests to PlaquetteLayer

--- src/tqec/compile/blocks/layers/atomic/plaquettes_test.py
+++ src/tqec/compile/blocks/layers/atomic/plaquettes_test.py
@@ -1,10 +1,13 @@
+import itertools
+
 import pytest
 
-from tqec.compile.blocks.enums import SpatialBlockBorder
+from tqec.compile.blocks.enums import SpatialBlockBorder, TemporalBlockBorder
 from tqec.compile.blocks.layers.atomic.plaquettes import PlaquetteLayer
 from tqec.plaquette.library.empty import empty_square_plaquette
 from tqec.plaquette.plaquette import Plaquettes
 from tqec.templates._testing import FixedTemplate
+from tqec.templates.qubit import QubitTemplate
 from tqec.utils.exceptions import TQECException
 from tqec.utils.frozendefaultdict import FrozenDefaultDict
 from tqec.utils.scale import LinearFunction, PhysicalQubitScalable2D
@@ -60,3 +63,77 @@ def test_scalable_shape() -> None:
     ).scalable_shape == PhysicalQubitScalable2D(
         LinearFunction(0, 17), LinearFunction(0, 17)
     )
+
+
+@pytest.mark.parametrize("borders", [(border,) for border in SpatialBlockBorder])
+def test_with_spatial_borders_trimmed(borders: tuple[SpatialBlockBorder, ...]) -> None:
+    template = QubitTemplate()
+    plaquettes = Plaquettes(
+        FrozenDefaultDict(
+            {
+                i + 1: empty_square_plaquette()
+                for i in range(template.expected_plaquettes_number)
+            },
+            default_factory=empty_square_plaquette,
+        )
+    )
+    layer = PlaquetteLayer(template, plaquettes)
+    all_indices = frozenset(plaquettes.collection.keys())
+    expected_plaquette_indices = all_indices - frozenset(
+        itertools.chain.from_iterable(
+            frozenset(template.get_border_indices(border.to_template_border()))
+            for border in borders
+        )
+    )
+    assert (
+        frozenset(
+            layer.with_spatial_borders_trimmed(borders).plaquettes.collection.keys()
+        )
+        == expected_plaquette_indices
+    )
+
+
+def test_with_temporal_borders_trimmed() -> None:
+    template = FixedTemplate([[1]])
+    plaquettes = Plaquettes(
+        FrozenDefaultDict({}, default_factory=empty_square_plaquette)
+    )
+    layer = PlaquetteLayer(template, plaquettes)
+    assert layer.with_temporal_borders_trimmed([]) == layer
+    assert layer.with_temporal_borders_trimmed([TemporalBlockBorder.Z_NEGATIVE]) is None
+    assert layer.with_temporal_borders_trimmed([TemporalBlockBorder.Z_POSITIVE]) is None
+    assert (
+        layer.with_temporal_borders_trimmed(
+            [TemporalBlockBorder.Z_NEGATIVE, TemporalBlockBorder.Z_POSITIVE]
+        )
+        is None
+    )
+
+
+def test_with_temporal_borders_replaced() -> None:
+    template = FixedTemplate([[1]])
+    plaquettes = Plaquettes(
+        FrozenDefaultDict({}, default_factory=empty_square_plaquette)
+    )
+    layer = PlaquetteLayer(template, plaquettes)
+    replacement_template = FixedTemplate([[2]])
+    replacement_plaquettes = Plaquettes(
+        FrozenDefaultDict({}, default_factory=empty_square_plaquette)
+    )
+    replacement_layer = PlaquetteLayer(replacement_template, replacement_plaquettes)
+
+    assert layer.with_temporal_borders_replaced({}) == layer
+    for replacement in [None, layer, replacement_layer]:
+        assert (
+            layer.with_temporal_borders_replaced(
+                {TemporalBlockBorder.Z_NEGATIVE: replacement}
+            )
+            == replacement
+        )
+    with pytest.raises(TQECException):
+        layer.with_temporal_borders_replaced(
+            {
+                TemporalBlockBorder.Z_NEGATIVE: None,
+                TemporalBlockBorder.Z_POSITIVE: replacement_layer,
+            }
+        )
