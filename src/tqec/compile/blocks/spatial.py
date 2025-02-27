e9d33440612b73799abd83d30854be98624c5127
Adrien Suau <adrien.suau+github@protonmail.com>
2025-02-27 16:11:30 +0000

    Fix pre-commit

--- src/tqec/compile/blocks/spatial.py
+++ src/tqec/compile/blocks/spatial.py
@@ -15,10 +15,10 @@ EXPECTED_SPATIAL_BORDER_WIDTH: Final[int] = 2
 At the moment, we need to ensure that removing one spatial border from any given
 layer will trim out a band of qubits with a known width. Some computations in
 the code base indirectly depends on the fact that this value is 2 for historical
-reasons. 
+reasons.
 
-Even though we do not need to in the forseeable future, changing that value 
-will likely lead to various errors in the code base. 
+Even though we do not need to in the foreseeable future, changing that value
+will likely lead to various errors in the code base.
 """
 
 
