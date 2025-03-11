052240f2492e806cd83bfbabaf39cb19da3dea1b
Adrien Suau <adrien.suau+github@protonmail.com>
2025-03-11 19:59:51 +0000

    Implement utilities in LayoutLayer to get circuits

--- src/tqec/compile/blocks/layers/atomic/layout.py
+++ src/tqec/compile/blocks/layers/atomic/layout.py
@@ -1,19 +1,37 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Final, Iterable
+from functools import cached_property
+from typing import Final, Iterable, TypeGuard
 
-import stim
 from typing_extensions import override
 
+from tqec.circuit.qubit_map import QubitMap
+from tqec.circuit.schedule.circuit import ScheduledCircuit
 from tqec.compile.blocks.enums import SpatialBlockBorder
 from tqec.compile.blocks.layers.atomic.base import BaseLayer
-from tqec.compile.blocks.positioning import LayoutPosition2D
+from tqec.compile.blocks.layers.atomic.plaquettes import PlaquetteLayer
+from tqec.compile.blocks.positioning import (
+    LayoutCubePosition2D,
+    LayoutPipePosition2D,
+    LayoutPosition2D,
+)
+from tqec.compile.generation import generate_circuit
+from tqec.plaquette.plaquette import Plaquettes
+from tqec.templates.enums import TemplateBorder
+from tqec.templates.layout import LayoutTemplate
 from tqec.utils.exceptions import TQECException
+from tqec.utils.position import BlockPosition2D, Direction3D, Shift2D
 from tqec.utils.scale import PhysicalQubitScalable2D
 
 DEFAULT_SHARED_QUBIT_DEPTH_AT_BORDER: Final[int] = 1
 """Default number of qubits that are shared between two neighbouring layers."""
+
+
+def contains_only_plaquette_layers(
+    layers: dict[LayoutPosition2D, BaseLayer],
+) -> TypeGuard[dict[LayoutPosition2D, PlaquetteLayer]]:
+    return all(isinstance(layer, PlaquetteLayer) for layer in layers.values())
 
 
 @dataclass(frozen=True)
@@ -28,6 +46,19 @@ class LayoutLayer(BaseLayer):
             raise TQECException(
                 f"An instance of {type(self).__name__} should have at least one layer."
             )
+
+    @cached_property
+    def bounds(self) -> tuple[BlockPosition2D, BlockPosition2D]:
+        xs = [pos._x for pos in self.layers.keys()]
+        ys = [pos._y for pos in self.layers.keys()]
+        minx, maxx = min(xs), max(xs)
+        miny, maxy = min(ys), max(ys)
+        # We know for sure that the minima and maxima above are located on cube
+        # positions and so are multiples of 2.
+        return (
+            BlockPosition2D(minx // 2, miny // 2),
+            BlockPosition2D(maxx // 2, maxy // 2),
+        )
 
     @property
     @override
@@ -59,5 +90,71 @@ class LayoutLayer(BaseLayer):
             and self.layers == value.layers
         )
 
-    def to_circuit(self) -> stim.Circuit:
-        raise NotImplementedError("")
+    def to_template_and_plaquettes(self) -> tuple[LayoutTemplate, Plaquettes]:
+        if not contains_only_plaquette_layers(self.layers):
+            raise NotImplementedError(
+                f"Found a layer that is not an instance of {PlaquetteLayer.__name__}. "
+                "Detector computation is not implemented (yet) for this case."
+            )
+        cubes: dict[BlockPosition2D, PlaquetteLayer] = {
+            pos.to_block_position(): layer
+            for pos, layer in self.layers.items()
+            if isinstance(pos, LayoutCubePosition2D)
+        }
+        template_dict: Final = {pos: layer.template for pos, layer in cubes.items()}
+        plaquettes_dict = {pos: layer.plaquettes for pos, layer in cubes.items()}
+
+        # Add plaquettes from each pipe to the plaquette_dict.
+        pipes: dict[tuple[BlockPosition2D, BlockPosition2D], PlaquetteLayer] = {
+            pos.to_pipe(): layer
+            for pos, layer in self.layers.items()
+            if isinstance(pos, LayoutPipePosition2D)
+        }
+        for (u, v), pipe_layer in pipes.items():
+            pipe_direction = Direction3D.from_neighbouring_positions(
+                u.to_3d(), v.to_3d()
+            )
+            u_border: TemplateBorder
+            v_border: TemplateBorder
+            match pipe_direction:
+                case Direction3D.X:
+                    u_border, v_border = TemplateBorder.LEFT, TemplateBorder.RIGHT
+                case Direction3D.Y:
+                    # TODO: check that this is the correct order.
+                    u_border, v_border = TemplateBorder.BOTTOM, TemplateBorder.TOP
+                case Direction3D.Z:
+                    raise TQECException("Should not happen. This is a logical error.")
+
+            # Updating plaquettes in plaquettes_dict
+            for pos, (cube_border, pipe_border) in [
+                (u, (v_border, u_border)),
+                (v, (u_border, v_border)),
+            ]:
+                plaquette_indices_mapping = pipe_layer.template.get_border_indices(
+                    pipe_border
+                ).to(template_dict[pos].get_border_indices(cube_border))
+                plaquettes_dict[pos] = plaquettes_dict[pos].with_updated_plaquettes(
+                    {
+                        plaquette_indices_mapping[pipe_plaquette_index]: plaquette
+                        for pipe_plaquette_index, plaquette in pipe_layer.plaquettes.collection.items()
+                    }
+                )
+
+        template = LayoutTemplate(template_dict)
+        return template, template.get_global_plaquettes(plaquettes_dict)
+
+    def to_circuit(self, k: int, global_qubit_map: QubitMap) -> ScheduledCircuit:
+        template, plaquettes = self.to_template_and_plaquettes()
+        scheduled_circuit = generate_circuit(template, k, plaquettes)
+        # Shift the qubits of the returned scheduled circuit
+        mincube, _ = self.bounds
+        eshape = self.element_shape.to_shape_2d(k)
+        shift = Shift2D(mincube.x * eshape.x, mincube.y * eshape.y)
+        shifted_circuit = scheduled_circuit.map_to_qubits(lambda q: q + shift)
+        # Relabel the qubit indices
+        local_qubit_map = shifted_circuit.qubit_map
+        qubit_indices_map = {
+            local_qubit_map[q]: global_qubit_map[q] for q in local_qubit_map.qubits
+        }
+        final_circuit = shifted_circuit.map_qubit_indices(qubit_indices_map)
+        return final_circuit
