558bf5564359056738c639a86ee7caa8571f2465
Adrien Suau <adrien.suau+github@protonmail.com>
2025-03-17 09:49:42 +0000

    Add docstrings to LayerTree

--- src/tqec/compile/tree/tree.py
+++ src/tqec/compile/tree/tree.py
@@ -16,6 +16,11 @@ from tqec.utils.exceptions import TQECException
 
 class QubitLister(NodeWalker):
     def __init__(self, k: int):
+        """Keeps in memory all the qubits used by the nodes explored.
+
+        Args:
+            k: scaling factor used to explore the quantum circuits.
+        """
         super().__init__()
         self._k = k
         self._seen_qubits: set[GridQubit] = set()
@@ -31,6 +36,7 @@ class QubitLister(NodeWalker):
 
     @property
     def seen_qubits(self) -> set[GridQubit]:
+        """Returns all the qubits seen when exploring."""
         return self._seen_qubits
 
 
@@ -40,6 +46,20 @@ class LayerTree:
         root: SequencedLayers,
         annotations: Mapping[int, LayerTreeAnnotations] | None = None,
     ):
+        """Represents a computation as a tree.
+
+        Note:
+            It is expected that the root is a
+            :class:`~tqec.compile.blocks.layer.composed.sequenced.SequencedLayers`
+            with each of its children representing the computation on a timespan
+            equivalent to the timespan of a cube.
+
+        Args:
+            root: root node of the tree.
+            annotations: a mapping from positive integers representing the value
+                of ``k``, the scaling factor, to annotations computed for that
+                value of ``k``.
+        """
         self._root = LayerNode(root)
         self._annotations = dict(annotations) if annotations is not None else {}
 
@@ -75,6 +95,20 @@ class LayerTree:
     def generate_circuit(
         self, k: int, include_qubit_coords: bool = True
     ) -> stim.Circuit:
+        """Generate the quantum circuit representing ``self``.
+
+        This method first annotates the tree according to the provided arguments
+        and then use these annotations to generate the final quantum circuit.
+
+        Args:
+            k: scaling factor.
+            include_qubit_coords: whether to include ``QUBIT_COORDS`` annotations
+                in the returned quantum circuit or not. Default to ``True``.
+
+        Returns:
+            a ``stim.Circuit`` instance implementing the computation described
+            by ``self``.
+        """
         self._annotate_circuits(k)
         self._annotate_qubit_map(k)
         self._annotate_detectors(k)
