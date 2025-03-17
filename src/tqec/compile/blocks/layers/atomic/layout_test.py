fe58b925568f0d75ed42fae70486ce41d58eb735
Adrien Suau <adrien.suau+github@protonmail.com>
2025-03-17 12:08:41 +0000

    Add tests for LayoutLayer.to_template_and_plaquettes

--- src/tqec/compile/blocks/layers/atomic/layout_test.py
+++ src/tqec/compile/blocks/layers/atomic/layout_test.py
@@ -7,6 +7,7 @@ from tqec.compile.blocks.layers.atomic.plaquettes import PlaquetteLayer
 from tqec.compile.blocks.positioning import LayoutPosition2D
 from tqec.plaquette.library.empty import empty_square_plaquette
 from tqec.plaquette.plaquette import Plaquettes
+from tqec.templates.layout import LayoutTemplate
 from tqec.templates.qubit import QubitTemplate
 from tqec.utils.exceptions import TQECException
 from tqec.utils.frozendefaultdict import FrozenDefaultDict
@@ -71,4 +72,45 @@ def test_scalable_shape(plaquette_layer: PlaquetteLayer) -> None:
     )
     assert layer.scalable_shape == PhysicalQubitScalable2D(
         2 * LOGICAL_QUBIT_SIDE - 1, 13 * (LOGICAL_QUBIT_SIDE - 1) + 1
+    )
+
+
+def test_to_template_and_plaquettes_single(plaquette_layer: PlaquetteLayer) -> None:
+    pos1 = LayoutPosition2D.from_block_position(BlockPosition2D(0, 0))
+    layer = LayoutLayer({pos1: plaquette_layer}, LOGICAL_QUBIT_SHAPE)
+    template, plaquettes = layer.to_template_and_plaquettes()
+
+    assert isinstance(template, LayoutTemplate)
+    assert len(template._layout) == 1
+    pos_layout, template_layout = next(iter(template._layout.items()))
+    assert pos_layout == BlockPosition2D(0, 0)
+    assert type(template_layout) is type(plaquette_layer.template)
+    assert plaquettes == plaquette_layer.plaquettes
+
+
+def test_to_template_and_plaquettes_multiple(plaquette_layer: PlaquetteLayer) -> None:
+    pos1 = LayoutPosition2D.from_block_position(BlockPosition2D(0, 0))
+    pos2 = LayoutPosition2D.from_block_position(BlockPosition2D(-1, 12))
+    layer = LayoutLayer(
+        {pos1: plaquette_layer, pos2: plaquette_layer}, LOGICAL_QUBIT_SHAPE
+    )
+    template, plaquettes = layer.to_template_and_plaquettes()
+
+    assert isinstance(template, LayoutTemplate)
+    assert len(template._layout) == 2
+    assert set(template._layout.keys()) == {
+        pos1.to_block_position(),
+        pos2.to_block_position(),
+    }
+    for t in template._layout.values():
+        assert type(t) is type(plaquette_layer.template)
+    assert plaquettes.collection == (
+        plaquettes.collection
+        | FrozenDefaultDict(
+            {
+                i + plaquette_layer.template.expected_plaquettes_number: plaq
+                for i, plaq in plaquettes.collection.items()
+            },
+            default_factory=plaquettes.collection.default_factory,
+        )
     )
