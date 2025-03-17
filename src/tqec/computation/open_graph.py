aead5bf82a70d8029e5708c7554a72e5b7267eb8
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-03-18 00:03:00 +0800

    Compute the generators with the small correlation surface area, likely work but need refactor and tests

--- src/tqec/computation/open_graph.py
+++ src/tqec/computation/open_graph.py
@@ -1,6 +1,7 @@
 """Utilities for working with graphs with open ports."""
 
 from dataclasses import dataclass
+from functools import reduce
 from itertools import combinations
 
 import stim
@@ -46,38 +47,75 @@ def fill_ports_for_simulation(graph: BlockGraph) -> list[FilledGraph]:
     """
     if graph.num_ports == 0:
         raise TQECException("The provided graph has no open ports.")
-    # Get the stabilizer generators for the graph
-    # Here we simply use the canonical stabilizer generators of the stabilizer
-    # tableau of the graph. Better generator selection can be made in the future,
-    # e.g. using the generators with the smallest correlation surface area.
-    tableau = open_graph_to_stabilizer_tableau(graph)
-    stabilizers = tableau.to_stabilizers(canonicalize=True)
+    correlation_surfaces = graph.find_correlation_surfaces()
+    stabilizers = [s.external_stabilizer_on_graph(graph) for s in correlation_surfaces]
+    stabilizer_to_correlation: dict[str, CorrelationSurface] = {
+        stabilizer: correlation_surface
+        for stabilizer, correlation_surface in zip(stabilizers, correlation_surfaces)
+    }
+    num_generators = len(stabilizers[0])
+    identity = "I" * num_generators
+    # powerset of the generators
+    for r in range(2, len(stabilizers) + 1):
+        for comb in combinations(stabilizers, r):
+            stabilizer = reduce(
+                _multiply_unsigned_paulis,
+                comb,
+                identity,
+            )
+            if stabilizer != identity and stabilizer not in stabilizer_to_correlation:
+                correlation_surface = reduce(
+                    lambda a, b: a + b,
+                    [stabilizer_to_correlation[s] for s in comb],
+                )
+                stabilizer_to_correlation[stabilizer] = correlation_surface
 
-    def is_compatible(s1: stim.PauliString, s2: stim.PauliString) -> bool:
-        for i in s1.pauli_indices():
-            if s2[i] not in [0, s1[i]]:
+    stabilizers_ordered_by_area = sorted(
+        stabilizer_to_correlation.keys(),
+        key=lambda s: stabilizer_to_correlation[s].area(),
+    )
+    # find a complete set of generators, starting from the smallest area
+    generators: list[str] = []
+    generators_stim: list[stim.PauliString] = []
+    for i, stabilizer in enumerate(stabilizers_ordered_by_area):
+        stim_pauli_string = stim.PauliString(stabilizer)
+        if i == 0:
+            generators.append(stabilizer)
+            generators_stim.append(stim_pauli_string)
+            continue
+        if not _op_is_in_the_group(stim_pauli_string, generators_stim):
+            generators.append(stabilizer)
+            generators_stim.append(stim_pauli_string)
+        if len(generators) == num_generators:
+            break
+
+    def is_compatible(s1: str, s2: str) -> bool:
+        for i, j in zip(s1, s2):
+            if i == "I" or j == "I":
+                continue
+            if i != j:
                 return False
         return True
 
     # min-clique-cover reduce to graph coloring problem on the complement graph
-    nodes = list(range(len(stabilizers)))
+    nodes = list(range(len(generators)))
     g: nx.Graph[int] = nx.Graph()
     g.add_nodes_from(nodes)
     for i, j in combinations(nodes, 2):
-        if not is_compatible(stabilizers[i], stabilizers[j]):
+        if not is_compatible(generators[i], generators[j]):
             g.add_edge(i, j)
     coloring = nx.algorithms.coloring.greedy_color(g)
-    cliques: dict[int, list[stim.PauliString]] = {}
+    cliques: dict[int, list[str]] = {}
     for node, color in coloring.items():
-        cliques.setdefault(color, []).append(stabilizers[node])
+        cliques.setdefault(color, []).append(generators[node])
 
     def ports_basis_for_clique(
-        supported_stabilizers: list[stim.PauliString],
+        supported_stabilizers: list[str],
     ) -> list[str]:
-        port_ops = ["I"] * len(supported_stabilizers[0])
+        port_ops = ["I"] * num_generators
         for s in supported_stabilizers:
-            for i in s.pauli_indices():
-                port_ops[i] = "IXYZ"[s[i]]
+            for i, p in enumerate(s):
+                port_ops[i] = p
         # For the ports with operator I, we can choose any basis
         # Here we choose the Z basis for consistency
         return ["Z" if p == "I" else p for p in port_ops]
@@ -105,20 +143,35 @@ def fill_ports_for_simulation(graph: BlockGraph) -> list[FilledGraph]:
             fg.fill_ports({port: fill_kind})
         assert fg.num_ports == 0
 
-        clique_stabilizers = [str(s)[1:].replace("_", "I") for s in clique]
-        correlation_surfaces = fg.find_correlation_surfaces()
-        filtered_surfaces = [
-            s
-            for s in correlation_surfaces
-            if s.external_stabilizer_on_graph(graph) in clique_stabilizers
-        ]
-        assert len(filtered_surfaces) == len(clique_stabilizers)
-
         filled_graphs.append(
             FilledGraph(
                 graph=fg,
-                stabilizers=clique_stabilizers,
-                observables=filtered_surfaces,
+                stabilizers=clique,
+                observables=[stabilizer_to_correlation[s] for s in clique],
             )
         )
     return filled_graphs
+
+
+def _multiply_unsigned_paulis(p1: str, p2: str) -> str:
+    from pyzx.pauliweb import multiply_paulis
+
+    return "".join(multiply_paulis(p1[i], p2[i]) for i in range(len(p1)))
+
+
+def _op_is_in_the_group(
+    pauli_string: stim.PauliString,
+    basis: list[stim.PauliString],
+) -> bool:
+    # Decompose into the basis by inverting its tableau.
+    tableau = stim.Tableau.from_stabilizers(
+        basis,
+        allow_redundant=False,
+        allow_underconstrained=True,
+    )
+    inv = tableau.inverse(unsigned=True)
+    out_paulis = inv(pauli_string)
+    for q in range(len(basis), len(out_paulis)):
+        if out_paulis[q] != 0:
+            return False
+    return True
