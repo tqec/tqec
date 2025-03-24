30985a63ca65598accc1c124773157fcd946059d
zhao-yilun-zyl <dilved1014@gmail.com>
2025-03-24 12:26:26 +0800

    test: add L shape test

--- src/tqec/compile/compile_test.py
+++ src/tqec/compile/compile_test.py
@@ -110,3 +110,53 @@ def test_compile_two_same_blocks_connected_in_space(
     assert dem.num_detectors == 2 * (d**2 - 1) + (d + 1 + 2 * (d**2 - 1)) * (d - 1)
     assert dem.num_observables == 1
     assert len(dem.shortest_graphlike_error()) == d
+
+
+@pytest.mark.parametrize(
+    ("spec", "kinds", "k"),
+    itertools.product(
+        ALL_SPECS.keys(),
+        (
+            ("ZXZ", "OXZ"),
+            ("ZXX", "ZOX"),
+            ("XZX", "OZX"),
+            ("XZZ", "XOZ"),
+        ),
+        (1,),
+    ),
+)
+def test_compile_L_shape_in_space_time(
+    spec: str, kinds: tuple[str, str], k: int
+) -> None:
+    d = 2 * k + 1
+    g = BlockGraph("L-shape Blocks Experiment")
+    cube_kind, space_pipe_kind = kinds[0], kinds[1]
+    time_pipe_type = PipeKind.from_str(kinds[0][:2] + "O")
+    p1 = Position3D(1, 2, 0)
+    space_shift = [0, 0, 0]
+    space_shift[PipeKind.from_str(space_pipe_kind).direction.value] = 1
+    p2 = p1.shift_by(*space_shift)
+    p3 = p2.shift_by(dz=1)
+    g.add_cube(p1, cube_kind)
+    g.add_cube(p2, cube_kind)
+    g.add_cube(p3, cube_kind)
+    g.add_pipe(p1, p2, space_pipe_kind)
+    g.add_pipe(p2, p3, time_pipe_type)
+
+    cube_builder, pipe_builder = ALL_SPECS[spec]
+    correlation_surfaces = g.find_correlation_surfaces()
+    assert len(correlation_surfaces) == 1
+    compiled_graph = compile_block_graph(
+        g, cube_builder, pipe_builder, correlation_surfaces
+    )
+    circuit = compiled_graph.generate_stim_circuit(
+        k, noise_model=NoiseModel.uniform_depolarizing(0.001), manhattan_radius=2
+    )
+
+    dem = circuit.detector_error_model()
+    assert (
+        dem.num_detectors
+        == 2 * (d**2 - 1) + (d + 1 + 2 * (d**2 - 1)) * (d - 1) + (d**2 - 1) * d
+    )
+    assert dem.num_observables == 1
+    assert len(dem.shortest_graphlike_error()) == d
