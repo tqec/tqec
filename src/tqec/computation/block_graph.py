ac0ecc5856b6136167deb892b7ca1dd208245ba9
Yiming Zhang <61700160+inmzhang@users.noreply.github.com>
2025-03-25 10:32:43 +0800

    Correct space time volume definition (#543)
    The definition of "spacetime volume" used throughout Austin's lectures
    is "the number of non-port cubes the computation has".
    
    Previously, it was defined as "the size of the bounding box of the block
    graph":
    
    
    https://github.com/tqec/tqec/blob/06420e1b53f227c2f1b6bcb18524f1e41a3c7d03/src/tqec/computation/block_graph.py#L113-L119
    
    This PR renames it as `bounding_box_size` and add the method
    `spacetime_volume` with the correct definition.

--- src/tqec/computation/block_graph.py
+++ src/tqec/computation/block_graph.py
@@ -89,6 +89,11 @@ class BlockGraph:
         return len([node for node in self.cubes if node.is_port])
 
     @property
+    def num_half_y_cubes(self) -> int:
+        """Number of half Y cubes in the graph."""
+        return len([node for node in self.cubes if node.is_y_cube])
+
+    @property
     def ordered_ports(self) -> list[str]:
         """Get the labels of the ports in the alphabetical order."""
         return sorted(self._ports.keys())
@@ -110,8 +115,22 @@ class BlockGraph:
         """Get the positions occupied by the cubes in the graph."""
         return list(self._graph.nodes)
 
-    def spacetime_volume(self) -> tuple[int, int, int]:
+    @property
+    def spacetime_volume(self) -> float:
         """Return the spacetime volume of the computation.
+
+        A port cube and the pipes have no spacetime volume. A half Y cube has a
+        spacetime volume of 0.5. Other cubes have a spacetime volume of 1. The
+        spacetime volume of the block graph is the sum of the spacetime volumes
+        of all the cubes in the graph.
+
+        Returns:
+            The spacetime volume of the computation.
+        """
+        return self.num_cubes - self.num_ports - self.num_half_y_cubes * 0.5
+
+    def bounding_box_size(self) -> tuple[int, int, int]:
+        """Return the size of the bounding box of the computation structure.
 
         Returns:
             A tuple of three integers representing the width along the X, Y, and Z
