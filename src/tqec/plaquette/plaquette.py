5d9f29d4448d1dc1c2b3bb6dae6679ced0ba0c0f
Adrien Suau <adrien.suau+github@protonmail.com>
2025-04-08 07:21:44 +0000

    Implement new projection methods on Plaquette

--- src/tqec/plaquette/plaquette.py
+++ src/tqec/plaquette/plaquette.py
@@ -6,6 +6,7 @@ from typing import Callable, Collection, Iterable, Literal, Mapping
 
 from typing_extensions import override
 
+from tqec.circuit.qubit import GridQubit
 from tqec.circuit.schedule import ScheduledCircuit
 from tqec.plaquette.debug import PlaquetteDebugInformation
 from tqec.plaquette.enums import PlaquetteOrientation
@@ -75,6 +76,73 @@ class Plaquette:
     def __str__(self) -> str:
         return self.name
 
+    def project_on_data_qubit_indices(
+        self, data_qubits_indices: list[int]
+    ) -> Plaquette:
+        """Project the plaquette on the provided qubits and return a new
+        plaquette with the remaining qubits and circuit.
+
+        This method is useful for deriving a boundary plaquette from a integral
+        plaquette.
+
+        Args:
+            data_qubits: data-qubit indices that will be kept in the returned
+                plaquette. Follow the standard indexing::
+
+                    1 ----- 2
+                    |       |
+                    |   0   |
+                    |       |
+                    3 ----- 4
+
+        Returns:
+            A new plaquette with projected qubits and circuit. The qubits are
+            updated to only keep the provided ``data_qubits_indices``. The
+            circuit is also updated to only use the kept qubits and empty
+            moments with the corresponding schedules are removed.
+        """
+        kept_qubits = [
+            q
+            for i, q in self.qubits.data_qubits_with_indices
+            if i in data_qubits_indices
+        ]
+        return self.project_on_data_qubits(kept_qubits)
+
+    def project_on_data_qubits(self, data_qubits: list[GridQubit]) -> Plaquette:
+        """Project the plaquette on the provided qubits and return a new
+        plaquette with the remaining qubits and circuit.
+
+        This method is useful for deriving a boundary plaquette from a integral
+        plaquette.
+
+        Args:
+            data_qubits: qubits that will be kept in the returned plaquette.
+
+        Returns:
+            A new plaquette with projected qubits and circuit. The qubits are
+            updated to only keep the provided ``data_qubits``. The circuit is
+            also updated to only use the kept qubits and empty moments with the
+            corresponding schedules are removed.
+        """
+        removed_data_qubit_indices = [
+            i for i, q in self.qubits.data_qubits_with_indices if q in data_qubits
+        ]
+        new_plaquette_qubits = PlaquetteQubits(data_qubits, self.qubits.syndrome_qubits)
+        new_scheduled_circuit = self.circuit.filter_by_qubits(
+            new_plaquette_qubits.all_qubits
+        )
+        debug_info = self.debug_information
+        if debug_info is not None:
+            debug_info = debug_info.with_data_qubits_removed(removed_data_qubit_indices)
+
+        return Plaquette(
+            f"{self.name}_" + "_".join(map(str, removed_data_qubit_indices)),
+            new_plaquette_qubits,
+            new_scheduled_circuit,
+            self.mergeable_instructions,
+            debug_info,
+        )
+
     def project_on_boundary(
         self, projected_orientation: PlaquetteOrientation
     ) -> Plaquette:
@@ -107,6 +175,9 @@ class Plaquette:
         debug_info = self.debug_information
         if debug_info is not None:
             debug_info = debug_info.project_on_boundary(projected_orientation)
+
+        # Note: this method is not implemented with project_on_data_qubits
+        # because we can set a more meaningful name here.
         return Plaquette(
             f"{self.name}_{projected_orientation.name}",
             new_plaquette_qubits,
