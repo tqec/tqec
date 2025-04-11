d138cab2adeb6732a213539e797afacbadaebb8a
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-04-11 21:26:10 +0800

    update test script

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from tqec import BlockGraph, compile_block_graph
-from tqec.utils.noise_model import NoiseModel
 from tqec.utils.position import Position3D
 
 
 g = BlockGraph("Test Temporal Hadamard")
-n1 = g.add_cube(Position3D(0, 0, 0), "ZXZ")
-n2 = g.add_cube(Position3D(0, 0, 1), "XZX")
+n1 = g.add_cube(Position3D(0, 0, 0), "XZZ")
+n2 = g.add_cube(Position3D(0, 0, 1), "ZXX")
 g.add_pipe(n1, n2)
 
 compiled = compile_block_graph(g)
-print(compiled.generate_crumble_url(k=2, add_polygons=True))
-# circuit = compiled.generate_stim_circuit(k=2, noise_model=NoiseModel.uniform_depolarizing(0.001))
-# print(len(circuit.shortest_graphlike_error()))
+print(compiled.generate_crumble_url(k=3, add_polygons=True, manhattan_radius=0))
