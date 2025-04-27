479c072d4e134a7e714b935b4cefa0eec9d73d28
Zhaoyilunnn <dilved1014@gmail.com>
2025-04-27 14:57:37 +0800

    docs: finalize steane code

--- src/tqec/gallery/steane_encoding.py
+++ src/tqec/gallery/steane_encoding.py
@@ -2,12 +2,13 @@
 
 from pathlib import Path
 from tqec.computation.block_graph import BlockGraph
+from tqec.utils.enums import Basis
 
 ASSETS_FOLDER = Path(__file__).resolve().parents[3] / "assets"
 
 
 # TODO:
-def steane_encoding() -> BlockGraph:
+def steane_encoding(observable_basis: Basis | None = None) -> BlockGraph:
     """Create a block graph that represents a Steane encoding circuit.
     The block graph is created from a DAE file that describes the circuit.
 
@@ -15,6 +16,10 @@ def steane_encoding() -> BlockGraph:
         A :py:class:`~tqec.computation.block_graph.BlockGraph` instance.
     """
     graph = BlockGraph.from_dae_file(ASSETS_FOLDER / "steane_encoding.dae")
-
     filled_graphs = graph.fill_ports_for_minimal_simulation()
-    return filled_graphs[0].graph
+    assert len(filled_graphs) == 2
+    if observable_basis == Basis.X:
+        return filled_graphs[0].graph
+    elif observable_basis == Basis.Z:
+        return filled_graphs[1].graph
+    return graph
