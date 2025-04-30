293fdedf56d5fe1282ff07442c89f70664ac10f0
BSchelpe <arabella.schelpe@gmail.com>
2025-04-30 14:35:38 +0100

    comment formatting
    Co-authored-by: Adrien Suau <adrien.suau+github@protonmail.com>

--- src/tqec/simulation/generation.py
+++ src/tqec/simulation/generation.py
@@ -132,7 +132,7 @@ def generate_sinter_tasks(
             ``database_path``.
         database_path: specify where to save to after the calculation, when augmented.
             This defaults to tqec.utils.paths.DEFAULT_DETECTOR_DATABASE_PATH if
-            not specified. If detector_database is not passed in, the code attempts to
+            not specified. If ``detector_database`` is not passed in, the code attempts to
             retrieve the database from this location. The user may pass in the path
             either in str format, or as a Path instance.
         do_not_use_database: if True, even the default database will not be used.
