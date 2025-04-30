bee6a0ea417641696358b32c18629185ce907978
BSchelpe <arabella.schelpe@gmail.com>
2025-04-30 14:41:35 +0100

    comment formatting
    Co-authored-by: Adrien Suau <adrien.suau+github@protonmail.com>

--- src/tqec/simulation/generation.py
+++ src/tqec/simulation/generation.py
@@ -61,7 +61,7 @@ def generate_stim_circuits_with_detectors(
             ``database_path``.
         database_path: specify where to save to after the calculation, when augmented.
             This defaults to :class:`.DEFAULT_DETECTOR_DATABASE_PATH` if
-            not specified. If detector_database is not passed in, the code attempts to
+            not specified. If ``detector_database`` is not passed in, the code attempts to
             retrieve the database from this location. The user may pass in the path
             either in str format, or as a Path instance.
         do_not_use_database: if True, even the default database will not be used.
