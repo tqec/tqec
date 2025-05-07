b42022c3ca80a8e6344a56ccffc0fdc4239a5fca
Adrien Suau <nelimee.dev+github@protonmail.com>
2025-05-07 09:15:54 +0200

    Update src/tqec/compile/specs/library/generators/fixed_parity.py
    Co-authored-by: Yiming Zhang <61700160+inmzhang@users.noreply.github.com>

--- src/tqec/compile/specs/library/generators/fixed_parity.py
+++ src/tqec/compile/specs/library/generators/fixed_parity.py
@@ -374,22 +374,15 @@ class FixedParityConventionGenerator:
         vsched = FixedParityConventionGenerator.VSCHED[is_reversed]
         hsched = FixedParityConventionGenerator.HSCHED[is_reversed]
         return {
-            Basis.X: {
+            basis: {
                 Orientation.VERTICAL: RPNGDescription.from_string(
-                    " ".join(f"{r}x{s}{m}" for r, s, m in zip(rs, vsched, ms))
+                    " ".join(f"{r}{basis.value.lower()}{s}{m}" for r, s, m in zip(rs, vsched, ms))
                 ),
                 Orientation.HORIZONTAL: RPNGDescription.from_string(
-                    " ".join(f"{r}x{s}{m}" for r, s, m in zip(rs, hsched, ms))
+                    " ".join(f"{r}{basis.value.lower()}{s}{m}" for r, s, m in zip(rs, hsched, ms))
                 ),
-            },
-            Basis.Z: {
-                Orientation.VERTICAL: RPNGDescription.from_string(
-                    " ".join(f"{r}z{s}{m}" for r, s, m in zip(rs, vsched, ms))
-                ),
-                Orientation.HORIZONTAL: RPNGDescription.from_string(
-                    " ".join(f"{r}z{s}{m}" for r, s, m in zip(rs, hsched, ms))
-                ),
-            },
+            }
+            for basis in Basis
         }
 
     def get_3_body_rpng_descriptions(
