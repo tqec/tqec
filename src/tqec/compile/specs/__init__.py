d57cbbd97aee3f82aea5b7742ab9feb655a3751f
Adrien Suau <adrien.suau+github@protonmail.com>
2025-05-08 15:31:35 +0000

    Bring back __init__.py files with docstrings

---
+++ src/tqec/compile/specs/__init__.py
@@ -0,0 +1,3 @@
+"""Defines the data-structures used to obtain correct
+:class:`tqec.compile.block.CompiledBlock` instances for different sets of
+plaquettes."""
---
+++ src/tqec/computation/__init__.py
@@ -0,0 +1,5 @@
+"""Defines the data structures to represent the logical computations.
+
+This module provides high-level abstractions to represent the fault-
+tolerant logical computations protected by surface code.
+"""
