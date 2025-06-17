775bf363207b4b81b888fc9bd6c968cd06332a71
Nelimee <12374487+nelimee@users.noreply.github.com>
2025-06-17 14:45:54 +0000

    Fix call to stim.Circuit.append("TICK")

--- src/tqec/compile/specs/library/generators/fixed_bulk.py
+++ src/tqec/compile/specs/library/generators/fixed_bulk.py
@@ -54,10 +54,10 @@ def make_fixed_bulk_realignment_plaquette(
             cx_schedule = [1, 2, 3, 5]
     circuit = stim.Circuit()
     circuit.append(f"R{mq_reset.value}", qubits.syndrome_qubits_indices, [])
-    circuit.append("TICK")
+    circuit.append("TICK", [], [])
     for targets in cx_targets:
         circuit.append("CX", targets, [])
-        circuit.append("TICK")
+        circuit.append("TICK", [], [])
     circuit.append(f"M{mq_measurement.value}", qubits.syndrome_qubits_indices, [])
     circuit.append("H", qubits.data_qubits_indices, [])
     schedule = [0, *cx_schedule, 6]
