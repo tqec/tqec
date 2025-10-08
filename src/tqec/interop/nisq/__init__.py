b16b5181c557a4cbe5aa9d233a48c04313090eda
J <80164315+jbolns@users.noreply.github.com>
2025-10-08 11:54:10 +0300

    Delete interops/nisq

--- src/tqec/interop/nisq/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Interoperability with Noisy Intermediate Scale Quantum (NISQ) circuit design frameworks."""
-
-from tqec.interop.nisq.load import (
-    load_nisq_circuit_as_qasm as load_nisq_circuit_as_qasm,
-)
