8c370a9c68b70b44d46b7eb2fe3ad6b3a5b24169
Sam Burdick <samuelmburdick@gmail.com>
2026-03-09 22:32:49 -0400

    Create database when path is provided

--- src/tqec/compile/tree/tree.py
+++ src/tqec/compile/tree/tree.py
@@ -258,7 +258,6 @@ class LayerTree:
         manhattan_radius: int = 2,
         detector_database: DetectorDatabase | None = None,
         database_path: Path | None = DEFAULT_DETECTOR_DATABASE_PATH,
-        do_not_use_database: bool = False,
         only_use_database: bool = False,
         lookback: int = 2,
         reschedule_measurements: bool = True,
@@ -287,7 +286,6 @@ class LayerTree:
                 This defaults to :data:`.DEFAULT_DETECTOR_DATABASE_PATH` if
                 not specified. If detector_database is not passed in, the code attempts to
                 retrieve the database from this location.
-            do_not_use_database: if ``True``, even the default database will not be used.
             only_use_database: if ``True``, only detectors from the database
                 will be used. An error will be raised if a situation that is not
                 registered in the database is encountered.
@@ -303,6 +301,8 @@ class LayerTree:
             by ``self``.
 
         """
+        if detector_database is None and database_path is not None and database_path.exists():
+            detector_database = DetectorDatabase.from_file(database_path)
         # Enable parallel processing only if the detector database is empty or None,
         # as current parallelization is effective only in this case.
         # If we later support efficient parallelism with a populated database,
