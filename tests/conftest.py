6f9a1044bec6d90cc08debce7fb9b186b822c31e
Sam Burdick <samuelmburdick@gmail.com>
2026-04-02 17:55:16 -0400

    refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,5 @@ SLOW_TEST_TIMEOUT = 300
 def pytest_collection_modifyitems(items):
     """Increase timeouts for tests marked as slow."""
     for item in items:
-        marker = item.get_closest_marker("slow")
-        if marker:
+        if item.get_closest_marker("slow"):
             item.add_marker(pytest.mark.timeout(SLOW_TEST_TIMEOUT))
